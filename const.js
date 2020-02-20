4f70d4d00c1c557eb650360eba9648705925edce
leung666 <15822198383@163.com>
2020-02-20 22:17:39 +0800

    提交电子书项目后台代码

---
+++ const.js
@@ -0,0 +1,31 @@
+const resUrl = 'http://192.168.1.107:8089';
+const mp3FilePath = '/Users/liyongjie/Desktop/html/ebook/mp3';
+const category = [
+    'Biomedicine',
+    'BusinessandManagement',
+    'ComputerScience',
+    'EarthSciences',
+    'Economics',
+    'Engineering',
+    'Education',
+    'Environment',
+    'Geography',
+    'History',
+    'Laws',
+    'LifeSciences',
+    'Literature',
+    'SocialSciences',
+    'MaterialsScience',
+    'Mathematics',
+    'MedicineAndPublicHealth',
+    'Philosophy',
+    'Physics',
+    'PoliticalScienceAndInternationalRelations',
+    'Psychology',
+    'Statistics'
+]
+module.exports = {
+    resUrl,
+    category,
+    mp3FilePath
+}
