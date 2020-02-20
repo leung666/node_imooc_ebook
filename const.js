cda0d817d9c96885ffc380ca77ef73be799b41ef
leung666 <15822198383@163.com>
2020-02-20 23:03:58 +0800

    添加环境变量设置

--- const.js
+++ const.js
@@ -1,5 +1,13 @@
-const resUrl = 'http://192.168.1.107:8089';
-const mp3FilePath = '/Users/liyongjie/Desktop/html/ebook/mp3';
+const env = require('env')
+let resUrl
+let mp3FilePath
+if(env==='dev'){
+    resUrl = 'http://192.168.1.107:8089';
+    mp3FilePath = '/Users/liyongjie/Desktop/html/ebook/mp3';
+}else if(env === 'prod'){
+    resUrl = 'http://39.107.66.67:8089';
+    mp3FilePath = '/root/nginx/upload/mp3';
+}
 const category = [
     'Biomedicine',
     'BusinessandManagement',
